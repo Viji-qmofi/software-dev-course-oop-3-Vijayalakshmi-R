4921378a24a9bb2d83d12c0dcae80d220c0a0d7f
Viji-qmofi <vijiramu@gmail.com>
2026-01-06 17:16:10 -0600

    completed coding and passed all 8 tests.

---
+++ src/main/java/org/example/Album.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Album extends LibraryItem {
+    protected int trackCount;
+
+    public Album(String title, String author, int year, int trackCount) {
+        super(title, author, year);
+        this.trackCount = trackCount;
+    }
+
+    public int getTrackCount() {
+        return trackCount;
+    }
+    @Override
+    public String toString() {
+        return ("Album: " + this.title + " by " + this.author + " (" + this.year + ") - " + this.trackCount + " tracks");
+    }
+}
